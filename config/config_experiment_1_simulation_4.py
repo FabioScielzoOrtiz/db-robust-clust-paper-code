0db7f8db937670197e988424d0532edf11624fb2
fabio <fabio.scielzoortiz@gmail.com>
2025-07-31 08:24:34 +0000

    running experiments for simulation 3 and 4

---
+++ config/config_experiment_1_simulation_4.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def get_config():
+    
+    config = {
+        'frac_sample_sizes': np.array([0.0005, 0.005, 0.01, 0.05, 0.1, 0.20]),
+        'n_clusters': 3,
+        'method': 'pam',
+        'init': 'heuristic',
+        'max_iter': 100,
+        'p1': 4,
+        'p2': 2,
+        'p3': 2,
+        'd1': 'robust_mahalanobis',
+        'd2': 'jaccard',
+        'd3': 'hamming',
+        'robust_method': 'trimmed',
+        'alpha': 0.05,
+        'epsilon': 0.05,
+        'n_iters': 20,
+        'VG_sample_size': 1000,
+        'VG_n_samples': 5
+    }
+
+    return config
